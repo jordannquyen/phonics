9ff7a1aad8083765a168e61d90175fb4a0e5c862
Jordan Nguyen <32782303+jordannquyen@users.noreply.github.com>
2021-01-03 19:21:51 -0800

    added instructions

--- src/components/Middle.tsx
+++ src/components/Middle.tsx
@@ -4,13 +4,13 @@ type Cluster = {
     selected:boolean
 }
 const Middle:Cluster[] = [
-    {id: 1, selected: true, cluster: 'o'},
-    {id: 2, selected: true, cluster: 'a'},
+    {id: 1, selected: true, cluster: 'a'},
+    {id: 2, selected: true, cluster: 'e'},
     {id: 3, selected: true, cluster: 'i'},
-    {id: 4, selected: true, cluster: 'u'},
-    {id: 5, selected: true, cluster: 'e'},
-    {id: 6, selected: true, cluster: 'ea'},
-    {id: 7, selected: true, cluster: 'oa'},
-    {id: 8, selected: true, cluster: 'ai'},
+    {id: 4, selected: true, cluster: 'o'},
+    {id: 5, selected: true, cluster: 'u'},
+    {id: 6, selected: true, cluster: 'ai'},
+    {id: 7, selected: true, cluster: 'ea'},
+    {id: 8, selected: true, cluster: 'oa'},
 ]
 export default Middle;
